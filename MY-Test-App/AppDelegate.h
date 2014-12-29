ad91e3f7b51a759dcd1979e6d9f9d54628385f85
Office Depot <officedepot@Offices-MacBook-Pro-2.local>
2014-12-29 10:23:41 -0500

    Initial Commit

---
+++ MY-Test-App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MY-Test-App
+//
+//  Created by Office Depot on 12/29/14.
+//  Copyright (c) 2014 KWilk83. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
